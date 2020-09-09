b8485377a4af69cf61c56bffd2f1a7f31f74cf24
yang xiang <xiangyang_xmu@qq.com>
2020-09-09 17:43:46 +0800

    Submit P3.sh

---
+++ homework/HW1/P3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+grep '[0-9]' apollo13.txt | wc -l
+grep --help | grep "\-c, \-\-count"
+ls | grep '\.py$' | wc -l
+find . -type f -exec ls -l {} \; | grep -E '^\-.{6}\-\-.' | wc -l
+ls -l | grep -E '^[-d].{6}\-\-.' | wc -l
