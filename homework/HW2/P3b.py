93cf256e02879fa4cd07844143818e90cf59c034
yang xiang <xiangyang_xmu@qq.com>
2020-09-27 21:24:34 +0800

    Edit P3b.py

--- homework/HW2/P3b.py
+++ homework/HW2/P3b.py
@@ -12,7 +12,9 @@ def make_withdraw(balance):
 
 
 print("This won't work, since if we try to update balance within the inner function, balance will be local "
-      "and resolved within the inner function (nearest scope), so it has nothing to do with outer function any more.")
+      "and resolved within the inner function (nearest scope), so it has nothing to do with outer function any more. "
+      "And here we use balance in 'if' before updating it, causing an UnboundLocalError "
+      "(reference it before assigment).")
 wd = make_withdraw(5000)
 print(wd(500))
 print(wd(1000))
