1100249ccc7d21a3177c24f3d2253e7beb957dfb
yang xiang <xiangyang_xmu@qq.com>
2020-09-30 11:37:16 +0800

    Update HW2/P4_animation.py, HW2/HW2-final/P4_animation.py

--- homework/HW2/HW2-final/P4_animation.py
+++ homework/HW2/HW2-final/P4_animation.py
@@ -14,7 +14,6 @@ def clock_r(r):
 
 
 fig = plt.figure(figsize=(6, 6))
-fig.canvas.draw()
 
 while datetime.datetime.now().second != 0:
     CurrentDT = datetime.datetime.now()
@@ -43,11 +42,11 @@ while datetime.datetime.now().second != 0:
     x_second, y_second = second_hand(theta_second)
 
     # Plot the clock
+    plt.cla()
     plt.plot([0, x_hour], [0, y_hour], linewidth=4)
     plt.plot([0, x_minute], [0, y_minute], linewidth=2)
     plt.plot([0, x_second], [0, y_second], linewidth=1)
     plt.axis([-length_second, length_second, -length_second, length_second])
     plt.axis('off')
-    plt.show()
+    fig.canvas.draw()
     plt.pause(0.1)
-    plt.cla()
